0557b64aa6857b3c42fa1db557e5e49e37c43402
RanjanSharma <ranjansharmadto@gmail.com>
2023-06-28 16:10:43 +0530

    Added a attractive table using bootstrap

--- frontend/react-todos/src/App.js
+++ frontend/react-todos/src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload...
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5 ">
+      <div className="card">
+        <div className="card-header"> Your To do List</div>
+        <div className="card-body">
+          <table className="table table-hover">
+            <thead className="table-primary">
+              <tr>
+                <th scope="col">S.No.</th>
+                <th scope="col">Description</th>
+                <th scope="col">Assigned</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>Feed Dog</td>
+                <td>Ranjan</td>
+              </tr>
+              <tr>
+                <th scope="row">2</th>
+                <td>Get Haircut</td>
+                <td>Ranjan</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
