5ee7a76d7713c1066b342c2e2d6f73566c246a1c
RanjanSharma <ranjansharmadto@gmail.com>
2023-07-21 18:08:00 +0530

    Frontend: Bug Fix in checkout page
    When book copies is 0, now user can't be able to checkout

--- frontend/library-app/src/layout/BookCheckoutPage/Checkout.tsx
+++ frontend/library-app/src/layout/BookCheckoutPage/Checkout.tsx
@@ -6,20 +6,32 @@ export const Checkout: React.FC<{
   currentCheckedBook: number;
   isBookChecked: Boolean;
   isAuthenticated: any;
-  checkoutBook:any;
+  checkoutBook: any;
 }> = (props) => {
   function checkoutBookRender() {
     if (props.isAuthenticated) {
-      if (!props.isBookChecked && props.currentCheckedBook < 5)
-        return ( <button type="button" className="btn btn-primary" onClick={()=>{props.checkoutBook()}}> Checkout </button> );
-      else if (props.isBookChecked)
-        return (
-          <h6 className="text-success">
-            Congrats, Book Checkout Successfully, enjoy!
-          </h6>
-        );
-      else if (!props.isBookChecked)
-        return <h6 className="text-danger">Too Many Books Checked out!</h6>;
+        if(props.book?.copiesAvailable === undefined || props.book.copiesAvailable <= 0)
+            return <h6 className="text-danger">Out Of Stock!</h6>
+        else if (!props.isBookChecked && props.currentCheckedBook < 5 )
+            return (
+                <button
+                    type="button"
+                    className="btn btn-primary"
+                    onClick={() => {
+                    props.checkoutBook();
+                    }}
+                >
+                    Checkout
+                </button>
+                );
+        else if (!props.isBookChecked)
+            return <h6 className="text-danger">Too Many Books Checked out!</h6>;
+        else if (props.isBookChecked)
+            return (
+            <h6 className="text-success">
+                Congrats, Book Checkout Successfully, enjoy!
+            </h6>
+            );
     }
 
     return (
@@ -30,28 +42,35 @@ export const Checkout: React.FC<{
   }
 
   return (
-    <div className="card container ms-2" >
-        <div className="card-body">
-            <p> <b>{props.currentCheckedBook}/5</b> books Checked out</p>
-            <hr />
-            {props.book && props.book.copiesAvailable && props.book.copiesAvailable > 0 ?
-                <h4 className='text-success'>
-                    Available
-                </h4>
-                :
-                <h4 className='text-danger'>
-                    Wait List
-                </h4>
-            }
-            <div className="mb-3 row">
-                <span className="col"> <b>{props.book?.copies}</b> Copies</span>
-                <span className="col"> <b>{props.book?.copiesAvailable}</b> Available </span>
-            </div>
-            {checkoutBookRender()}
-            <hr />
-            <p>This number can change until placing order has been complete</p>
-            <p>Sign in to be able to leave a review</p>
+    <div className="card container ms-2">
+      <div className="card-body">
+        <p>
+          {" "}
+          <b>{props.currentCheckedBook}/5</b> books Checked out
+        </p>
+        <hr />
+        {props.book &&
+        props.book.copiesAvailable &&
+        props.book.copiesAvailable > 0 ? (
+          <h4 className="text-success">Available</h4>
+        ) : (
+          <h4 className="text-danger">Wait List</h4>
+        )}
+        <div className="mb-3 row">
+          <span className="col">
+            {" "}
+            <b>{props.book?.copies}</b> Copies
+          </span>
+          <span className="col">
+            {" "}
+            <b>{props.book?.copiesAvailable}</b> Available{" "}
+          </span>
         </div>
+        {checkoutBookRender()}
+        <hr />
+        <p>This number can change until placing order has been complete</p>
+        <p>Sign in to be able to leave a review</p>
+      </div>
     </div>
-  )
-}
+  );
+};
