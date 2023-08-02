a6ae89fb7c5f83e9f458a1a0732e8656335cb923
RanjanSharma <ranjansharmadto@gmail.com>
2023-08-02 12:54:31 +0530

    Alert Massage Bug Improved css

--- frontend/library-app/src/layout/Utils/AlertMassage.tsx
+++ frontend/library-app/src/layout/Utils/AlertMassage.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 
-const AlertMassage:React.FC<{massage:any}> = (props) => {
+const AlertMassage: React.FC<{ massage: any }> = (props) => {
   return (
-        <div className="position-fixed top-0 end-0" style={{ zIndex: 9999, marginTop: "56px", marginRight: "10px" }}>
-          <div className="alert alert-success" role="alert">
-            {props.massage}
-          </div>
-        </div>
+    <div className="position-fixed top-0 end-0" style={{ zIndex: 9999, marginTop: "58px", marginRight: "3px" }}>
+      <div className="alert alert-success alert-dismissible fade show" role="alert">
+        <strong>{props.massage}</strong>
+        <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      </div>
+    </div>
   )
 }
 export default AlertMassage;
